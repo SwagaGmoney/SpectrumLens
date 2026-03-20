22417c24bacd9556f4a86334772cb6786463054f
Yash Gandhi <gyash557@gmail.com>
2026-03-19 21:49:41 -0500

    fixed routing to backend main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,15 @@
+import sys
 import os
+from pathlib import Path
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+
+
+
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
